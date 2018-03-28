597c9d82517ca8d8b9e1b0c8aa498cbe8ce4a977
alex <shu5388369@gmail.com>
2018-03-28 21:44:46 +0300

    исправление замечаний по стилю ruby style guide

--- Ideal_weight.rb
+++ Ideal_weight.rb
@@ -2,11 +2,12 @@ print "Введите ваше имя"
 name = gets.chomp
 
 print "Введите ваш рост"
-height = gets.chomp
+height = gets.chomp.to_i
 
-weight = height.to_i - 110
+weight = height - 110
+
 if weight >= 0
-puts "#{name}, Ваш вес #{weight} кг"
+	puts "#{name}, Ваш вес #{weight} кг"
 else 
-puts "#{name}, Ваш вес уже оптимальный" 
+	puts "#{name}, Ваш вес уже оптимальный" 
 end
--- quadratic_equation.rb
+++ quadratic_equation.rb
@@ -5,17 +5,17 @@ b = gets.to_f
 print "Введите коэффициент c = "
 c = gets.to_f
 
-d = b*b - 4*a*c
+d = b * b - 4 * a * c
 
-	if d < 0
-		puts "корней нет!"
-	elsif d > 0
-		x_1 = (-b + Math.sqrt(d))/(2*a) 
-		puts "первый корень уравнения x1=#{x_1}"
-		x_2 = (-b - Math.sqrt(d))/(2*a) 	
-		puts "второй корень уравнения x2=#{x_2}"	
-	else 
-		x = -b/(2*a)
-		puts "корни уравнения одинаковы и равны x1=x2=#{x}"
-	end
+if d < 0
+	puts "корней нет!"
+elsif d > 0
+	x_1 = (- b + Math.sqrt(d)) / (2 * a)
+	puts "первый корень уравнения x1=#{x_1}"
+	x_2 = (- b - Math.sqrt(d)) / (2 * a)
+	puts "второй корень уравнения x2=#{x_2}"
+else
+	x = -b / (2 * a)
+	puts "корни уравнения одинаковы и равны x1=x2=#{x}"
+end
 		
